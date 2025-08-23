d44816c1c3986968c03579c8ef9b3fc0189f2bfa
Vaibhs-11 <vaibhs.sh@gmail.com>
2025-08-23 12:34:47 +1000

    firestore persistent uid

--- lib/data/services/auth/firebase_auth_service.dart
+++ lib/data/services/auth/firebase_auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart' as fb_auth;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smartreceipt/data/services/auth/auth_service.dart';
 
 class FirebaseAuthService implements AuthService {
@@ -8,7 +9,7 @@ class FirebaseAuthService implements AuthService {
       : _auth = instance ?? fb_auth.FirebaseAuth.instance;
 
   final fb_auth.FirebaseAuth _auth;
-
+  static const String _uidKey = 'persisted_uid';
   AppUser? _mapUser(fb_auth.User? user) =>
       user == null ? null : AppUser(uid: user.uid, email: user.email);
 
@@ -19,11 +20,30 @@ class FirebaseAuthService implements AuthService {
 
   @override
   Future<AppUser?> signInAnonymously() async {
+    final prefs = await SharedPreferences.getInstance();
+    // Check if we already have a stored UID
+    final storedUid = prefs.getString(_uidKey);
+    if (storedUid != null) {
+      print('[signInAnonymously] Using stored UID: $storedUid');
+      // Return an AppUser with stored UID (simulate persistence)
+      return AppUser(uid: storedUid, email: null);
+    }
     final fb_auth.UserCredential cred = await _auth.signInAnonymously();
+    final uid = cred.user?.uid;
+    print('[signInAnonymously] New UID created: $uid');
+
+    if (uid != null) {
+      await prefs.setString(_uidKey, uid);
+    }
+
     return _mapUser(cred.user);
   }
 
-  @override
-  Future<void> signOut() => _auth.signOut();
+   @override
+  Future<void> signOut() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_uidKey); // clear persisted UID on sign out
+    return _auth.signOut();
+  }
 }
 
